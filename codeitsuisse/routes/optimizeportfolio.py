12d8d42c8ca7ea34d525bf2f99b3a6d62fbc168e
Sunny Lee <33865037+CSJLIMO@users.noreply.github.com>
2020-09-26 18:07:22 +0800

    Update optimizeportfolio.py

--- codeitsuisse/routes/optimizeportfolio.py
+++ codeitsuisse/routes/optimizeportfolio.py
@@ -11,23 +11,41 @@ logger = logging.getLogger(__name__)
 
 def get_ans(port_val, sigma_s, futures):
     best_hedge_ratio = 2
-    best_index = -1
+    best_hedge_ratio_index = -1
     for i in range(len(futures)):
         future = futures[i]
         hedge_ratio = future["CoRelationCoefficient"] * sigma_s / future["FuturePrcVol"]
         hedge_ratio = round(hedge_ratio, 3)
         if hedge_ratio < best_hedge_ratio:
             best_hedge_ratio = hedge_ratio
-            best_index = i
+            best_hedge_ratio_index = i
+            
+    best_sigma_f = 2
+    best_sigma_f_index = -1
+    for i in range(len(futures)):
+        future = futures[i]
+        if future["FuturePrcVol"] < best_sigma_f:
+            best_sigma_f = future["FuturePrcVol"]
+            best_sigma_f_index = i
+            
+    best_index = -1
+    if best_hedge_ratio_index == best_sigma_f_index:
+        best_index = best_hedge_ratio_index
+    else:
+        future1 = futures[best_hedge_ratio_index]
+        future2 = futures[best_sigma_f_index]
+        contracts1 = future1["CoRelationCoefficient"] * sigma_s / future1["CoRelationCoefficient"] * port_val / (future1["IndexFuturePrice"] * future1["Notional"])
+        contracts2 = future2["CoRelationCoefficient"] * sigma_s / future2["CoRelationCoefficient"] * port_val / (future2["IndexFuturePrice"] * future2["Notional"])
+        if contracts1 < contracts2:
+            best_index = best_hedge_ratio_index
+        else:
+            best_index = best_sigma_f_index
             
     name = futures[best_index]["Name"]
     contracts = best_hedge_ratio * port_val / (futures[best_index]["IndexFuturePrice"] * futures[best_index]["Notional"])
     contracts = int(round(contracts, 0))
     
     return {"HedgePositionName": name, "OptimalHedgeRatio": best_hedge_ratio, "NumFuturesContract": contracts}
-
-
-
 @app.route('/optimizedportfolio', methods=['POST'])
 def evaluate_optimizeportfolio():
     data = request.get_json();
@@ -43,6 +61,9 @@ def evaluate_optimizeportfolio():
 
     # logging.info("My result :{}".format(result))
     return jsonify({"outputs": outputs});
+
+
+
 
 
 
