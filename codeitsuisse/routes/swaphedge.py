df0c23e73df7d873918ac60c0eac22efb0d0dbc9
Sunny Lee <33865037+CSJLIMO@users.noreply.github.com>
2020-09-26 18:31:07 +0800

    Add files via upload

---
+++ codeitsuisse/routes/swaphedge.py
@@ -0,0 +1,25 @@
+import logging
+import json
+import numpy as np
+
+from flask import request, jsonify;
+
+from codeitsuisse import app;
+
+logger = logging.getLogger(__name__)
+    
+
+@app.route('/swaphedge', methods=['POST'])
+def evaluate_swaphedge():
+    data = request.get_json();
+    logging.info("data sent for evaluation {}".format(data))
+    
+    order = int(data["accu_order"] - data["our_position"])
+    
+
+    
+    logging.info("My result :{}".format(order))
+    return jsonify({"order": order});
+
+
+
