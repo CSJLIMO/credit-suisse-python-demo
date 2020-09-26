9d97b9ba9d9d241c32deb7181d08c32e52f597f2
Sunny Lee <33865037+CSJLIMO@users.noreply.github.com>
2020-09-27 01:39:26 +0800

    Update optimizeportfolio.py

--- codeitsuisse/routes/optimizeportfolio.py
+++ codeitsuisse/routes/optimizeportfolio.py
@@ -10,21 +10,25 @@ from codeitsuisse import app;
 logger = logging.getLogger(__name__)
 
 def my_round(num, k):
-    up = math.ceil(num * (10 ** k)) / (10 ** k)
-    down = math.floor(num * (10 ** k)) / (10 ** k)
-    dup = abs(num - up)
-    ddown = abs(num - down + 0.0000001)
-    if dup > ddown:
-        return down
-    return up
-
+    if k == 3:
+        num *= 1000
+        rem = num - int(num)
+        if rem >= 0.5:
+            return (int(num) + 1) / 1000
+        else:
+            return int(num) / 1000
+    rem = num - int(num)
+    if rem >= 0.5:
+        return (int(num) + 1)
+    else:
+        return int(num)
 
 def get_ans(port_val, sigma_s, futures):
     hedge_ratios = []
     sigma_fs = []
     for i in range(len(futures)):
         future = futures[i]
-        hedge_ratios.append(round(future["CoRelationCoefficient"] * sigma_s / future["FuturePrcVol"], 3))
+        hedge_ratios.append(my_round(future["CoRelationCoefficient"] * sigma_s / future["FuturePrcVol"], 3))
         sigma_fs.append(future["FuturePrcVol"])
         
     convex_ind = []
@@ -43,12 +47,12 @@ def get_ans(port_val, sigma_s, futures):
     best_contracts = 1000000000
     best_ind = -1
     for ind in convex_ind:
-        contracts = round(hedge_ratios[ind] * port_val / (futures[ind]["IndexFuturePrice"] * futures[ind]["Notional"]), 0)
+        contracts = my_round(hedge_ratios[ind] * port_val / (futures[ind]["IndexFuturePrice"] * futures[ind]["Notional"]), 0)
         if contracts < best_contracts:
             best_contracts = contracts
             best_ind = ind
             
-    name = futures[best_ind]["Name"]
+    name = futures[best_ind]["Name"]    
     return {"HedgePositionName": name, "OptimalHedgeRatio": hedge_ratios[best_ind], "NumFuturesContract": int(best_contracts + 0.3)}
             
     
@@ -69,6 +73,10 @@ def evaluate_optimizeportfolio():
 
     logging.info("My result :{}".format(outputs))
     return jsonify({"outputs": outputs});
+
+
+
+
 
 
 
